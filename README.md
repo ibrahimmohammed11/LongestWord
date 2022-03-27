77ddacc2e056056460870f56aeb9dcc5c2491464
ibrahimmohammed11 <ibrahimmohammed9979@gmail.com>
2022-03-28 00:02:07 +0200

    edit import

--- README.md
+++ README.md
@@ -9,6 +9,6 @@ Get longest word in any string easily
 then...
 
 ```
-import {findLongest} from "longestword";
+import findLongest from "longestwordinstring";
 console.log(findLongest("Js program to search largest word from a string"));
 ```
--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-function findLongest(str) {
+export default function findLongest(str) {
     let newArr = str.split(" ");
     let long = newArr.reduce((acc, current) =>  acc.length < current.length ? current : acc);
     return long;
 }
-
-
-module.exports.findLongest = findLongest;
-// console.log(findLongest("Js program to search largest word from a string"));
